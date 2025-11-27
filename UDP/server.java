55cc1e5944decbd3bea992dd4ec521dae88f1473
Nish344 <148205163+Nish344@users.noreply.github.com>
2025-11-27 15:44:20 +0530

    Create server.java

---
+++ UDP/server.java
@@ -0,0 +1,36 @@
+package pgm8;
+import java.net.*;
+
+class UDPServer {
+    public static void main(String[] args) throws Exception {
+
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+
+        byte[] receiveData = new byte[1024];
+        byte[] sendData;
+
+        while (true) {
+            System.out.println("Server is Up...");
+
+            DatagramPacket receivePacket =
+                    new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String sentence =
+                    new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+            System.out.println("RECEIVED: " + sentence);
+
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+
+            DatagramPacket sendPacket =
+                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+            serverSocket.send(sendPacket);
+        }
+    }
+}
